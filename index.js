e590c96ef4c41621aaddb50e0a8e312f6b38508d
Sunitha Iyadurai <>
2021-04-08 03:16:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const input = require('readline-sync');
+
+function getValidInput(prompt, isValid) {
+    
+    let userInput = input.question(prompt);
+
+    while (!isValid(userInput)) {
+      console.log("Invalid input. Try again.");
+      userInput = input.question(prompt);
+    }
+
+    return userInput;
+}
+
+// TODO 1: write a validator 
+// that ensures input starts with "a"
+let isValidName = function(name) {
+   if (name[0]=='a') {
+      return true;
+   }
+
+   return false;
+};
+
+console.log(getValidInput('Enter Name:', isValidName));
+// TODO 2: write a validator 
+// that ensures input is a vowel
+
+// Be sure to test your code! 
